b176056ca212ed194a95c00148eef4e598ce77c6
joseMercad0 <jose.mercado.c@tecsup.edu.pe>
2025-06-18 18:49:32 -0500

    udate send email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,19 +3,15 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
     //Create email transporter
    const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: parseInt(process.env.EMAIL_PORT), // 587 para Outlook
-    secure: process.env.EMAIL_SECURE === 'true', // false para puerto 587
+     host: process.env.EMAIL_HOST,         // smtp.gmail.com
+    port: parseInt(process.env.EMAIL_PORT), // 465
+    secure: process.env.EMAIL_SECURE === "true",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
     tls: {
         rejectUnauthorized: false
-    },
-     tls: {
-      ciphers: "SSLv3",
-      rejectUnauthorized: false,
     },
    });
    // Option for sending email
